6ebda79e0cecd0f9254b1fc6f03c68990fb69ca9
Pratik Kumar <pkumar@tavisca.com>
2017-07-12 13:14:42 +0530

    1st two Codejams

---
+++ InverseFactoringCodeJam/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Codejam
+{
+    class InverseFactoring
+    {
+        public int GetTheNumber(int[] factors)
+        {
+            int max = factors[0];
+            int min = max;
+            for (int i = 0; i < factors.Length; i++)
+            {
+                if (min > factors[i])
+                {
+                    min = factors[i];
+                }
+                if (max < factors[i])
+                {
+                    max = factors[i];
+                }
+            }
+            return min * max;
+        }
+
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            String input = Console.ReadLine();
+            InverseFactoring inverseFactoring = new InverseFactoring();
+            do
+            {
+                Thread th = new Thread(() =>
+                {
+                    try
+                    {
+
+                        string[] values = input.Split(',');
+                        int[] factors = Array.ConvertAll<string, int>(values, delegate (string s) { return Int32.Parse(s); });
+                        int validationOp = inverseFactoring.GetTheNumber(factors);
+                        Console.WriteLine(validationOp);
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception Occured" + ex.ToString());
+                    }
+                });
+                th.Start();
+                if (th.Join(1000) == false)
+                {
+                    Console.WriteLine("Timeout occured");
+                    th.Abort();
+                    return;
+                }
+                input = Console.ReadLine();
+
+            } while (input != "-1");
+        }
+
+        #endregion
+    }
+}
---
+++ TriFactorialCodeJam/TriFibonacci.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Codejam
+{
+    class TriFibonacci
+    {
+        public int Complete(int[] arr)
+        {
+            int missingIndex = 0;
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == -1)
+                    missingIndex = i;
+            }
+            if (missingIndex > 2)
+            {
+                result = arr[missingIndex - 3] + arr[missingIndex - 2] + arr[missingIndex - 1];
+                arr[missingIndex] = result;
+            }
+            else
+            {
+                result = arr[3]-(arr[0] + arr[1] + arr[2] + 1);
+                arr[missingIndex] = result;
+            }
+            if (result < 1) return -1;
+            for (int i=3;i<arr.Length;i++)
+            {
+                if (arr[i] != arr[i - 3] + arr[i - 2] + arr[i - 1])
+                    return -1;
+            }
+            return result;
+        }
+
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            String input = Console.ReadLine();
+            TriFibonacci triFibonacci = new TriFibonacci();
+            do
+            {
+                string[] values = input.Split(',');
+                int[] numbers = Array.ConvertAll<string, int>(values, delegate (string s) { return Int32.Parse(s); });
+                int result = triFibonacci.Complete(numbers);
+                Console.WriteLine(result);
+                input = Console.ReadLine();
+            } while (input != "-1");
+        }
+        #endregion
+    }
+}
