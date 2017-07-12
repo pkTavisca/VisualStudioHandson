d29158d5a12741aa1e9c55680e7079301e4001ba
Pratik Kumar <pkumar@tavisca.com>
2017-07-12 20:24:18 +0530

    anagram done

--- ACGT_CodeJam/Program.cs
+++ ACGT_CodeJam/Program.cs
@@ -6,8 +6,50 @@ namespace CodeJam
     {
         int MinChanges(int maxPeriod, string[] acgt)
         {
-            //Your code goes here
-            return 0;
+            string combined = "";
+            for (int i = 0; i < acgt.Length; i++)
+            {
+                combined += acgt[i];
+            }
+            int[] changesInAllPeriods = new int[maxPeriod];
+            for (int period = 1; period <= maxPeriod; period++)
+            {
+                int noOfChangesInPeriod = 0;
+                int maxGroups = (int)(Math.Ceiling(combined.Length / (double)period));
+                int[] noOfChangesInGroup = new int[maxGroups];
+                for (int group = 0; group < maxGroups; group++)
+                {
+                    for (int inPeriodIndex = 0; inPeriodIndex < period && group * period + inPeriodIndex < combined.Length; inPeriodIndex++)
+                    {
+                        int currentPosition = inPeriodIndex;
+                        while (currentPosition < combined.Length)
+                        {
+                            if (combined[currentPosition] != combined[period * group + inPeriodIndex])
+                            {
+                                noOfChangesInGroup[group]++;
+                            }
+                            currentPosition += period;
+                        }
+                    }
+                }
+                if (combined.Length % maxPeriod != 0) noOfChangesInGroup[maxGroups - 1] = noOfChangesInGroup[0];
+                noOfChangesInPeriod = MinInArray(noOfChangesInGroup);
+                changesInAllPeriods[period - 1] = noOfChangesInPeriod;
+            }
+            return MinInArray(changesInAllPeriods);
+        }
+
+        int MinInArray(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
         }
 
         #region Testing code Do not change
---
+++ AnagramsCodeJam/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace Codejam
+{
+    class Anagrams
+    {
+        int GetMaximumSubset(string[] words)
+        {
+            ArrayList finalWords = new ArrayList();
+            for (int i = 0; i < words.Length; i++)
+            {
+                finalWords.Add(words[i]);
+            }
+            for (int i = 0; i < finalWords.Count; i++)
+            {
+                for (int j = i + 1; j < finalWords.Count; j++)
+                {
+                    if (AreAnagrams((string)finalWords[i], (string)finalWords[j]))
+                    {
+                        finalWords.RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+            return finalWords.Count;
+        }
+
+        bool AreAnagrams(string str1, string str2)
+        {
+            return AreEqualMapping(CharacterMap(str1), CharacterMap(str2));
+        }
+
+        bool AreEqualMapping(Dictionary<char, int> charMap1, Dictionary<char, int> charMap2)
+        {
+            if (charMap1.Count != charMap2.Count) return false;
+            foreach (KeyValuePair<char, int> entry in charMap1)
+            {
+                if (!charMap2.ContainsKey(entry.Key)) return false;
+                if (charMap2[entry.Key] != entry.Value) return false;
+            }
+            return true;
+        }
+
+        Dictionary<char, int> CharacterMap(string str)
+        {
+            Dictionary<char, int> charMap = new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!charMap.ContainsKey(str[i]))
+                {
+                    charMap.Add(str[i], 1);
+                }
+                else
+                {
+                    charMap[str[i]] += 1;
+                }
+            }
+            return charMap;
+        }
+
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            String input = Console.ReadLine();
+            Anagrams anagramSolver = new Anagrams();
+            do
+            {
+                string[] words = input.Split(',');
+                Console.WriteLine(anagramSolver.GetMaximumSubset(words));
+                input = Console.ReadLine();
+            } while (input != "-1");
+        }
+        #endregion
+    }
+}
+
